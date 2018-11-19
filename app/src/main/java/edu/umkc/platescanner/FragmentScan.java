715823a27497d4ef43d6daee5b7762eaa2b072e4
Matthew Downs <mattcd96@gmail.com>
2018-11-18 20:42:24 -0600

    Adding login layout and class

--- app/src/main/java/edu/umkc/platescanner/FragmentScan.java
+++ app/src/main/java/edu/umkc/platescanner/FragmentScan.java
@@ -1,28 +1,25 @@
 package edu.umkc.platescanner;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.hardware.Camera;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 import edu.umkc.platescanner.scanner.CameraPreview;
-import edu.umkc.platescanner.scanner.ScanLicensePlate;
 
 public class FragmentScan extends Fragment {
 
@@ -79,6 +76,8 @@ public class FragmentScan extends Fragment {
                     public void onPictureTaken(byte[] data, Camera camera) {
                         try (FileOutputStream fos = new FileOutputStream(ANDROID_DATA_DIR + File.separatorChar + "runtime_data" + File.separatorChar + "temp_img_file")) {
                             fos.write(data);
+                        } catch (Exception e) {
+
                         }
                     }
                 });
